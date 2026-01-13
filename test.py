939aa635cca9fb1cf68e9a4b3b3d15201bde5488
David Schell <schelldavid.2003@gmail.com>
2026-01-13 16:24:52 +0100

    Added probabilities

--- test.py
+++ test.py
@@ -5,26 +5,31 @@ ONLY FOR TESTING PURPOSES
 WILL BE DELETED LATER!!!!!!
 """
 
-# 1. Setup
+# Setup
 tokenizer, model, device = load_model("google-bert/bert-base-cased")
 
-# 2. Test review
+# Test review
 text = "Worst movie ever. Completely boring and a waste of time."
 
-# 3. Tokenization
+# Tokenization
 inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True).to(device)
 
-# 4. Prediction (without gradient)
+# Prediction (without gradient)
 with torch.no_grad():
     outputs = model(**inputs)
     logits = outputs.logits
     
 print(logits)
 
-# 5. Interpret result
+# Interpret result
 # We take the highest value (Argmax)
 prediction_id = torch.argmax(logits, dim=-1).item()
 label = model.config.id2label[prediction_id]
 
+# Softmax probabilities
+probabilities = torch.nn.functional.softmax(logits, dim=-1)
+
+
 print(f"Text: {text}")
+print(f"Probabilities: Negative={probabilities[0][0].item():.4f}, Positive={probabilities[0][1].item():.4f}")
 print(f"Predicted Sentiment: {label}")
