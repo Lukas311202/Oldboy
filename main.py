26b851680ed25fe74d37ab195c843502d76d99fc
David Schell <schelldavid.2003@gmail.com>
2026-01-15 21:54:15 +0100

    main adjustments

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ if __name__ == "__main__":
         print("CUDA not found. Check your drivers.")
     
     MODEL_NAME = "google-bert/bert-base-cased"
+    DATA_PATH = "imdb_dataset.csv"
 
     # Train-test split
-    train_df, test_df = create_train_test_split(data="imdb_dataset.csv", text_column="review", label_column="sentiment",
+    train_df, test_df = create_train_test_split(data=DATA_PATH, text_column="review", label_column="sentiment",
                                                 test_size=0.2, seed=42, stratify=True)
     
     # Fine-tune the model
-    fine_tuned_model_path = fine_tune_loop(train_df=train_df, base_model=MODEL_NAME, epochs=3, batch_size=16, learning_rate=2e-5)
-
+    #fine_tuned_model_path = fine_tune_loop(train_df=train_df, base_model=MODEL_NAME, epochs=3, batch_size=16, learning_rate=2e-5)
+    fine_tuned_model_path = "fine_tuned_bert.pth"
     # Load the fine-tuned model 
     tokenizer, fine_tuned_model, device = load_fine_tuned_model(model_name=MODEL_NAME, model_path=fine_tuned_model_path)
     
@@ -30,14 +31,19 @@ if __name__ == "__main__":
     classification_report, confusion_matrix = model_evaluation(model=fine_tuned_model, test_df=test_df, tokenizer=tokenizer, device=device)
 
     """
-    Use classification_report and confusion_matrix to further fine-tuned-model evaluations (plotting etc.)
+    Use classification_report and confusion_matrix for further fine-tuned-model evaluations (plotting etc.) 
     """
 
     # Calculate word attributions
-    attribution_values_json = run_attributions(tokenizer=tokenizer, model=fine_tuned_model, train_df=test_df)
+    attribution_values_json = run_attributions(n_steps=10, save_every=50, tokenizer=tokenizer, model=fine_tuned_model, train_df=test_df)
 
     """
     Council decision, which words to consider as bullshit words
+    """
+
+    """
+    Loss function that incorporates word attributions to penalize the model for relying on bullshit words
+    Lambda tweaking
     """
 
 
